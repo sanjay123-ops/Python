5930e899d70cd240f4392a13e3fe179bf84687f4
sanjay123-ops <sanjayj.24.beai@acharya.ac.in>
2025-05-22 09:51:28 +0530

    Create CharecterPictureGrid.py

---
+++ CharecterPictureGrid.py
@@ -0,0 +1,15 @@
+#charactergrid
+print("Sanjay J,USN:1AY24AI100,SEC:O")
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print()
